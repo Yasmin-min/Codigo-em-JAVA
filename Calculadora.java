59ecd15c6f0dfd9e236138b2fdf473a21617bb3b
Yasmin Neumann <yasminneumann@hotmail.com>
2023-11-16 21:54:17 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -3,9 +3,8 @@ package calculadora;
 import java.util.Scanner;
 
 public class Calculadora {
-
+	static Scanner entrada = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
 		int opcao = 1;
 		
 		while (opcao != 0) { 
@@ -18,8 +17,8 @@ public class Calculadora {
 			System.out.print("O resultado da sua soma é "+resultado);
 			break;
 		case 2:
-			resultado = subitrair();
-			System.out.print("O resultado da sua subitração é "+resultado);
+			resultado = subtrair();
+			System.out.print("O resultado da sua subtração é "+resultado);
 			break;
 		case 3:
 			resultado = multiplicar();
@@ -43,7 +42,7 @@ public class Calculadora {
 		System.out.println("\n"
 				+ "\n-----MENU-----\n"
 				+ "\n1. Somar"
-				+ "\n2. Subitrair"
+				+ "\n2. Subtrair"
 				+ "\n3. Multiplicar "
 				+ "\n4. Dividir"
 				+ "\n0. Sair");
@@ -51,7 +50,6 @@ public class Calculadora {
 	}
 
 	public static double soma() {
-		Scanner entrada = new Scanner(System.in);
 		System.out.print("Digite o 1° número: ");
 		double n1 = entrada.nextDouble();
 		System.out.print("Digite o 2° número: ");
@@ -59,17 +57,15 @@ public class Calculadora {
 		double soma = n1+n2;
 		return soma;
 	}
-	public static double subitrair() {
-		Scanner entrada = new Scanner(System.in);
+	public static double subtrair() {
 		System.out.print("Digite o 1° número: ");
 		double n1 = entrada.nextDouble();
 		System.out.print("Digite o 2° número: ");
 		double n2 = entrada.nextDouble();
-		double subitrair = n1-n2;
-		return subitrair;
+		double subtrair = n1-n2;
+		return subtrair;
 	}
 	public static double multiplicar() {
-		Scanner entrada = new Scanner(System.in);
 		System.out.print("Digite o 1° número: ");
 		double n1 = entrada.nextDouble();
 		System.out.print("Digite o 2° número: ");
@@ -78,26 +74,19 @@ public class Calculadora {
 		return multiplicar;
 	}
 	public static double dividir() {
-		Scanner entrada = new Scanner(System.in);
 		double n1, n2;
 		while (true) {
-		System.out.print("Digite o divisor: ");
-		n1 = entrada.nextDouble();
-		if (n1==0) {
-			System.out.println("Não é possivel dividir 0, digite outro número");
-		}else {
-			break;
-		}
-		}
-		while (true) {
-		System.out.println("Digite o dividendo: ");
-		n2 = entrada.nextDouble();
-		if (n2==0){
-			System.out.println("Não possivel dividir um número por 0, digite outro número");
-		}else {
-			break;
-		}
-		}
+			System.out.println("Digite o dividendo: ");
+			n1 = entrada.nextDouble();
+			System.out.print("Digite o divisor: ");
+			n2 = entrada.nextDouble();
+			if (n1==0 || n2==0){
+				System.out.println("Não possui divisões com 0, digite outros valores");
+			}else {
+				break;
+			}
+			}
+		
 		double dividir = n1-n2;
 		return dividir;
 		
